b5a3ca63835829e89077ad64da1ab2e03cba411f
김가을 <rkdmf11103@naver.com>
2024-10-10 20:09:39 +0900

    저장버튼 추가
    apply 버튼 누르면 tmp_texture 폴더에 이미지 파일로 저장

---
+++ Test/Assets/Codes/ConfirmButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfirmButton : MonoBehaviour
+{
+    public DrawingCanvas drawingCanvas;
+    public Button confirmButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        confirmButton = GetComponent<Button>();
+
+        // 확인 버튼에 리스너 추가
+        if (confirmButton != null)
+        {
+            confirmButton.onClick.AddListener(OnConfirmButtonClick);
+        }
+        else
+        {
+            Debug.LogError("Confirm button is not assigned in the inspector!");
+        }
+
+        if (drawingCanvas == null)
+        {
+            Debug.LogError("DrawingCanvas reference is not set in the inspector!");
+        }
+    }
+
+    void OnConfirmButtonClick()
+    {
+        if (drawingCanvas != null)
+        {
+            drawingCanvas.SaveCanvasAsTexture();
+        }
+        else
+        {
+            Debug.LogError("Cannot save texture: DrawingCanvas reference is null!");
+        }
+    }
+}
